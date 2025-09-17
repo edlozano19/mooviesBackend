36162caca81886fae281b42605c20205178245d9
Eduardo <elozano2a@gmail.com>
2025-09-17 11:57:29 -0500

    Initial commit: Spring Boot Movies Backend project setup

---
+++ src/main/java/com/moovies/mooviesBackend/MooviesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moovies.mooviesBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooviesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MooviesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moovies/mooviesBackend/MooviesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moovies.mooviesBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooviesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
