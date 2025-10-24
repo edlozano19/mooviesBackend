c42b59cea3b0458cff8197ea13d738ef240030eb
Eduardo <elozano2a@gmail.com>
2025-10-24 16:04:09 -0500

    Created Entities and liquibase migrations

---
+++ src/main/java/com/moovies/mooviesBackend/movie/entity/UserWatchedList.java
@@ -0,0 +1,55 @@
+package com.moovies.mooviesBackend.movie.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import com.moovies.mooviesBackend.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+    name = "user_watched_list",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "movie_id"})
+)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserWatchedList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+
+    @CreatedDate
+    @Column(name = "added_at", nullable = false, updatable = false)
+    private LocalDateTime addedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        addedAt = LocalDateTime.now();
+    }
+}
